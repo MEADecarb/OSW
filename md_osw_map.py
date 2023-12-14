48054987027cec781e2cebe00a096d5d29c0909a
MEADecarb <152652613+MEADecarb@users.noreply.github.com>
2023-12-14 10:19:54 -0500

    Update md_osw_map.py

--- md_osw_map.py
+++ md_osw_map.py
@@ -1,7 +1,12 @@
 import folium
 import json
 import requests
-from branca.element import Template, MacroElement
+
+# Define a color palette
+color_palette = ["#2C557E", "#fdda25", "#B7DCDF", "#000000"]  # Fixed color format
+
+# Create a base map centered over Maryland
+m = folium.Map(location=[39.0458, -76.6413], zoom_start=8)
 
 # Function to add GeoJSON from a URL to a feature group with custom color and pop-up
 def add_geojson_from_url(geojson_url, name, color, map_obj):
@@ -16,43 +21,20 @@ def add_geojson_from_url(geojson_url, name, color, map_obj):
     )
 
     if name == "Offshore Wind Energy Planning":
+        # Custom popup for Offshore Wind Energy Planning
         fields = ['PROT_NUMBE', 'PROT_APRV_', 'BLOCK_NUMB', 'BLK_FED_AP', 'BLOCK_LAB', 'SUB_BLK']
         aliases = ['Protraction Number', 'Protraction Approval', 'Block Number', 'Block Approval', 'Block Label', 'SubBlock']
         geojson_layer.add_child(folium.GeoJsonPopup(fields=fields, aliases=aliases, labels=True))
     elif name == "MDOT SHA County Boundaries":
+        # Use 'County' as the label for 'COUNTY_NAME'
         geojson_layer.add_child(folium.GeoJsonPopup(fields=['COUNTY_NAME'], aliases=['County:'], labels=True))
     else:
+        # For other layers, list all available fields
         all_fields = list(geojson_data['features'][0]['properties'].keys())
         geojson_layer.add_child(folium.GeoJsonPopup(fields=all_fields, labels=True))
 
     geojson_layer.add_to(feature_group)
     feature_group.add_to(map_obj)
-
-# Function to add a legend to the map
-def add_legend(map_obj, color_palette, labels):
-    legend_html = '''
-    <div style="position: fixed; 
-                bottom: 50px; left: 50px; width: 150px; height: 90px; 
-                border:2px solid grey; z-index:9999; font-size:14px;
-                ">&nbsp; <b>Legend</b> <br>
-                &nbsp; {0} &nbsp; <i style="background:{1};opacity:0.7;"></i><br>
-                &nbsp; {2} &nbsp; <i style="background:{3};opacity:0.7;"></i><br>
-                &nbsp; {4} &nbsp; <i style="background:{5};opacity:0.7;"></i><br>
-    </div>
-    '''.format(labels[0], color_palette[0], 
-               labels[1], color_palette[1], 
-               labels[2], color_palette[2])
-
-    legend = MacroElement()
-    legend._template = Template(legend_html)
-
-    map_obj.get_root().add_child(legend)
-
-# Define a color palette
-color_palette = ["#2C557E", "#fdda25", "#B7DCDF", "#000000"]
-
-# Create a base map centered over Maryland
-m = folium.Map(location=[39.0458, -76.6413], zoom_start=8)
 
 # Add each GeoJSON source as a separate feature group with a color, label, and pop-up
 github_geojson_sources = [
@@ -64,11 +46,8 @@ for i, (url, name) in enumerate(github_geojson_sources):
     color = color_palette[i % len(color_palette)]
     add_geojson_from_url(url, name, color, m)
 
-# Define labels for your legend (these should correspond to your layers)
-legend_labels = ["Offshore Wind Energy Planning", "MDOT SHA County Boundaries", "Other Layer"]
-
-# Add legend to the map
-add_legend(m, color_palette, legend_labels)
+# Add Layer Control to toggle feature groups
+folium.LayerControl().add_to(m)
 
 # Display the map
 m
